347119b9c4df38be8dba467934004eb82e80c939
Solarus <micformweb@gmail.com>
2026-05-15 02:28:33 +0200

    up

--- src/Composants/Logo/index.jsx
+++ src/Composants/Logo/index.jsx
@@ -1,4 +1,4 @@
-import Logo from "../../img/logo512.png"
+import logoImage from "../../img/logo512.png"
 import "./logo.scss"
 
 
@@ -6,7 +6,7 @@ import "./logo.scss"
 export default function Logo() {
     return (
         <div className="logo">
-            <img src={Logo} alt="Logo" />
+            <img src={logoImage} alt="Logo" />
         </div>
     )
 }   
